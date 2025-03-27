7cb6ad73f6ef496866eaf511006107bd2adc071a
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-03-27 20:40:17 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,40 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
 
-	}
+		public static Boolean validIndex(int[] intArray, int index) {
+			try {
+				int temp  = intArray[index];
+				return true;
+
+			}
+
+			catch(ArrayIndexOutOfBoundsException e) {
+				return false;
+			}
+		}
+
+
+		public static int divide(int one, int two) {
+			try { 
+				return one/two;
+			}
+			catch(ArithmeticException e) {
+				return 0;
+			}
+		}
+
+		public static int safeConvertStringtoInt(String string) {
+			try { 
+				return Integer.parseInt(string);
+
+			}
+			catch (NumberFormatException e) {
+				return 0;
+			}
+		}
+
+
+	
 
 	
 
